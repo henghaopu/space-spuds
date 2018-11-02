d3c8f1e4ea00d42665a0ec038a0aebfe50062b04
Heng-Hao Pu <hpu@oswego.edu>
2018-11-02 00:58:45 -0400

    Add radio/checkbox in solo mission and rocket fuel

--- scripts.js
+++ scripts.js
@@ -1,20 +1,3 @@
-(function () {
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-    
-    for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var panel = this.nextElementSibling;
-            if (panel.style.maxHeight){
-                panel.style.maxHeight = null;
-            } else {
-                panel.style.maxHeight = panel.scrollHeight + "px";
-            } 
-        });
-    }
-})();
-
 let vm = new Vue({
     el: '#root',
     data: {
@@ -42,3 +25,20 @@ let vm = new Vue({
         }
     }
 });
+
+(function () {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+    
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight){
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            } 
+        });
+    }
+})();
